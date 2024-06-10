186f05e76ac659d63e5406e31939b34e851f2012
Mary Kilduff <marykilduff@Marys-MacBook-Pro-3.local>
2024-06-10 13:29:10 +0100

    linking

--- Route/src/main.js
+++ Route/src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Header from "./components/header/index.vue";
 
-import Header from './components/header/index.vue';
+const app = createApp(App);
 
-const app =  createApp(App);
-
-app.component('app-header',Header);
-app.mount('#app')
+app.component("app-header", Header);
+app.use(router);
+app.mount("#app");

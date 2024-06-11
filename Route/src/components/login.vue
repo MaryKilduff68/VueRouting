8939353bab6ea36cd5115fcb69b3be042f43d363
Mary Kilduff <marykilduff@Marys-MacBook-Pro-3.local>
2024-06-11 09:55:42 +0100

    some global route guards

---
+++ Route/src/components/login.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Login in</h1>
+</template>
--- Route/src/router/index.js
+++ Route/src/router/index.js
@@ -5,6 +5,7 @@ import Home from "@/components/home.vue";
 import Article from "@/components/articles/article.vue";
 import NotFound from "@/components/404.vue";
 import Notify from "@/components/notify.vue";
+import Login from "@/components/login.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,9 +21,27 @@ const router = createRouter({
       name: "contact",
     },
     { path: "/articles/:articleID", component: Article, props: true },
+    { path: "/login", component: Login },
     { path: "/:notFound(.*)", component: NotFound },
   ],
   linkActiveClass: "active",
+});
+
+router.beforeEach((to, from) => {
+  const isAuth = true;
+
+  if (to.path === "/") {
+    return true;
+  } else {
+    if ((to.path !== "/login") & !isAuth) {
+      return "/login";
+    }
+    if ((to.path === "/login") & isAuth) {
+      return "/";
+    }
+  }
+
+  return true;
 });
 
 export default router;
